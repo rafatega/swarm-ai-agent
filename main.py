b054945d580d34a94855a18c0226f4d0a6861bc2
RafaTega <rafael_tegazzini@hotmail.com>
2025-09-30 21:10:30 -0300

    Ajuste de prompt

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# streamlit run main.py
+# streamlit run main.py <- command to run the app
 
 if __name__ == '__main__':
     swarm_client = Swarm(OpenAI(api_key=OPENAI_API_KEY))
